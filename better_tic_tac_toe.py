a71f1a20108d129db8a2a61a7198418eb22610e6
Dieblerbua <elias.diebler@outlook.com>
2023-11-29 09:54:41 +0100

    finished win conditions, no letter input

--- better_tic_tac_toe.py
+++ better_tic_tac_toe.py
@@ -1,7 +1,6 @@
 player1 = input("Player 1, please state your name: ")
 player2 = input("Player 2, how may I call you? ")
 
-
 repeat = True
 player = 1
 lineCorrect = False
@@ -11,7 +10,6 @@ win = False
 gameboard = [[" "," "," "],[" "," "," "],[" "," "," "]]
 currentPlayer = player1
 currentSymbol = "X"
-
 
 def table():
     print("----------")
@@ -55,9 +53,12 @@ def turn():
 #choose line
     print(currentPlayer + ", in which line would you like to make your first move?")
     lineChoice = input()
+    if lineChoice.isdigit() == False:
+        print("This is not a number.")
+        return
     lineInt = int(lineChoice)
     linePos = lineInt - 1
-
+    
     if linePos < 0 or linePos > 2:
         print("Sorry, this line does not exist.")
         return
@@ -67,6 +68,9 @@ def turn():
 #choose column
     print("And in which position shall we place your " + currentSymbol + " ?")
     columnChoice = input()
+    if lineChoice.isdigit() == False:
+       print("This is not a number.")
+       return
     columnInt = int(columnChoice)
     columnPos = columnInt - 1
 
@@ -87,13 +91,24 @@ def possible():
      else:
           print("This move has already been made.")
           emptySpace = False
-
+    
 def winConditions():
+    global win
+    
     #lines
     for line in gameboard:
-        if gameboard == [currentSymbol, currentSymbol, currentSymbol]:
+        if line == [currentSymbol, currentSymbol, currentSymbol]:
             win = True
-            print("Well done, " + currentPlayer + ", you have won the game")
+    
+    #columns
+    if gameboard[0][columnPos] == gameboard[1][columnPos] == gameboard[2][columnPos]:
+        win = True
+    
+    #diagonal
+    if gameboard[0][0] and gameboard [1][1] and gameboard[2][2] == currentSymbol:
+        win = True
+    if gameboard[2][0] and gameboard [1][1] and gameboard[0][2] == currentSymbol:
+            win = True
 
 table()
 
@@ -103,15 +118,13 @@ while repeat:
     if lineCorrect and columnCorrect and emptySpace == True:
         gameboard[linePos][columnPos] = currentSymbol
         winConditions()
-        turnchanger()
+        if win == True:
+            repeat = False
+            print("Well done, " + currentPlayer + ", you have won the game!")
+        else: turnchanger()
     else: print("Please choose another position for your '" + currentSymbol + "'.")
     
     table()
-       
-    if win == True:
-        repeat = False
     
     lineCorrect = False
     columnCorrect = False
-
-    #TODO don´t allow letters
